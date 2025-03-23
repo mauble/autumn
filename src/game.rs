222653d2c181497c70b7b33862493f8dc3bd67d2
mauble <92347868+mauble@users.noreply.github.com>
2025-03-23 23:36:55 +0100

    Add tilemap rendering

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_ecs_tiled::prelude::*;
 
 use crate::player;
 
@@ -8,17 +9,31 @@ impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup_game.after(player::setup_player))
             .add_systems(Update, update_game)
+            .add_plugins(TiledMapPlugin::default())
             .add_plugins(player::PlayerPlugin);
     }
 }
 
-fn setup_game(mut commands: Commands, player_query: Query<&Transform, With<player::Player>>) {
+fn setup_game(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    player_query: Query<&Transform, With<player::Player>>,
+) {
     if let Ok(player_pos) = player_query.get_single() {
         commands.spawn((
             Transform::from_translation(player_pos.translation),
             Camera2d,
         ));
     }
+
+    commands.spawn((
+        Transform {
+            scale: Vec3::splat(5.0),
+            ..default()
+        },
+        TiledMapHandle(asset_server.load("maps/1.tmx")),
+        TiledMapAnchor::Center,
+    ));
 }
 
 fn update_game(
@@ -31,7 +46,7 @@ fn update_game(
             let current_pos = camera_pos.translation;
             let target_pos = player_pos.translation;
 
-            let lerp_factor = 5.0 * time.delta_secs();
+            let lerp_factor = 10. * time.delta_secs();
 
             camera_pos.translation = current_pos + (target_pos - current_pos) * lerp_factor;
         }
